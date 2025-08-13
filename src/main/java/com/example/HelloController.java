5684f309698a10a8de34aee0723346ee5492cc2c
alina-yur <alina.yurenko@oracle.com>
2025-08-13 16:20:35 +0200

    try RCP

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+
+@Controller
+public class HelloController {
+
+    @Get("/hello")
+    public String hello() {
+        return "Hello from GraalVM and Micronaut!🧑‍🚀";
+    }
+
+}
